a724ca2e815e838183f15547fe5d8065e799bfbc
Sydnie <sydnie.ritchie@jamcity.com>
2020-10-29 10:52:07 -0700

    fezzball well underway

---
+++ Inventory Management/Assets/BallBounce.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float thrust = 100.0f;
+    Vector3 direction;
+
+    void Start()
+    {
+        
+        rb = gameObject.GetComponent<Rigidbody>();
+        //transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+        direction = transform.up;
+        rb.AddForce(direction * thrust);
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ball hit something");
+        if (other.GetComponent<Laser>() != null) {
+            Debug.Log("ball hit a laser");
+            ChangeDirection();
+            //stop the laser
+        }
+    }
+    void ChangeDirection() { //this aint working well. they fall off the screen all the time real easy. Need to be stopped by the walls better.
+        Vector3 incomingVelocity = rb.velocity;
+        rb.velocity = new Vector3(Random.Range(0,1),Random.Range(0,1), Random.Range(0,1));
+        //  rb.angularVelocity = new Vector3(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+        //thrust *= -1;
+        rb.AddForce(incomingVelocity * -1
+            + new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), 0)*100);
+    }
+}
---
+++ Inventory Management/Assets/Laser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    float laserSpeed = .2f;
+    int direction;
+    bool grow = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -.02f);
+    }
+
+
+    void Update()
+    {
+        if (grow) {
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + laserSpeed, transform.localScale.z);
+            //rotation determines axis of movement
+            if (transform.localEulerAngles.z == 0) {
+                transform.position = new Vector3(transform.position.x, transform.position.y + laserSpeed/2 * direction, transform.position.z);
+
+            }
+            else
+                transform.position = new Vector3(transform.position.x + laserSpeed/2 * direction, transform.position.y , transform.position.z);
+
+        }
+        if (transform.localScale.y >= 20f) { //temp
+
+            grow = false;
+
+        }
+        if (!grow) {
+            GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.GetComponent<Laser>()!= null) {
+            if (other.GetComponent<Laser>().grow == false)
+            {
+                grow = false;
+                Debug.Log("laser hit another laser"); //works!
+            }
+            if (other.GetComponent<BallBounce>() != null) {
+                grow = false;
+                Debug.Log("laser hit ball");
+                //lose a life
+                // not triggering. because this is enter? and hmmmmm they both have rigid bodies. :( 
+
+            }
+        }
+        
+    }
+
+    internal void setDirection(int v)
+    {
+
+        direction = v;
+    }
+}
